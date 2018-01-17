ae36859350d80f888994ffb2d77bdcf14aa8cc4d
Juraj Fulir <juraj.fulir@microblink.com>
2018-01-17 11:19:49 +0100

    Force android plugin version to latest subversion of v6

--- initCordovaDemoApp.sh
+++ initCordovaDemoApp.sh
@@ -26,7 +26,7 @@ cd $APP_NAME
 cordova plugin add ../BlinkID --variable CAMERA_USAGE_DESCRIPTION="Camera permission is required for automated scanning"
 
 # add ios and android support to the project
-cordova platform add android
+cordova platform add android@6
 cordova platform add ios
 
 # copy content of the www folder
