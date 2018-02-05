29e56c36d6e4bcc6ded6c5376d4504b6cb9ccf29
Ivan Grce <ivan.grce@microblink.com>
2018-02-05 11:07:35 +0100

    [js] added UAE ID recognizers, result keys and result fields extraction

---
+++ www/js/common_keys.js
@@ -0,0 +1,12 @@
+//
+//  common_keys.js
+//  MicroBlink BlinkID library
+//
+//  Copyright (c) 2015 MicroBlink. All rights reserved.
+//
+
+
+//pragma mark - Keys for obtaining common data
+
+
+var kPPDataType = "PaymentDataType";
