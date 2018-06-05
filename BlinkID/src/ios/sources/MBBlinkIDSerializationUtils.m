24c919414cd956551912ef44c25544ea7cc96c98
Nenad Mikša <nenad.miksa@microblink.com>
2018-06-05 10:21:06 +0200

    use dictionary literal

--- BlinkID/src/ios/sources/MBBlinkIDSerializationUtils.m
+++ BlinkID/src/ios/sources/MBBlinkIDSerializationUtils.m
@@ -11,28 +11,26 @@
 @implementation MBBlinkIDSerializationUtils
 
 +(NSDictionary *) serializeMRZResult:(MBMRZResult *)mrzResult {
-    NSMutableDictionary *jsonMrz = [[NSMutableDictionary alloc] init];
-
-    [jsonMrz setObject:[NSNumber numberWithInteger:(mrzResult.documentType + 1)] forKey:@"documentType"];
-    [jsonMrz setObject:mrzResult.primaryID forKey:@"primaryId"];
-    [jsonMrz setObject:mrzResult.secondaryID forKey:@"secondaryId"];
-    [jsonMrz setObject:mrzResult.issuer forKey:@"issuer"];
-    [jsonMrz setObject:[MBSerializationUtils serializeMBDateResult:mrzResult.dateOfBirth] forKey:@"dateOfBirth"];
-    [jsonMrz setObject:mrzResult.documentNumber forKey:@"documentNumber"];
-    [jsonMrz setObject:mrzResult.nationality forKey:@"nationality"];
-    [jsonMrz setObject:mrzResult.gender forKey:@"gender"];
-    [jsonMrz setObject:mrzResult.documentCode forKey:@"documentCode"];
-    [jsonMrz setObject:[MBSerializationUtils serializeMBDateResult:mrzResult.dateOfExpiry] forKey:@"dateOfExpiry"];
-    [jsonMrz setObject:mrzResult.opt1 forKey:@"opt1"];
-    [jsonMrz setObject:mrzResult.opt2 forKey:@"opt2"];
-    [jsonMrz setObject:mrzResult.alienNumber forKey:@"alienNumber"];
-    [jsonMrz setObject:mrzResult.applicationReceiptNumber forKey:@"applicationReceiptNumber"];
-    [jsonMrz setObject:mrzResult.immigrantCaseNumber forKey:@"immigrantCaseNumber"];
-    [jsonMrz setObject:mrzResult.mrzText forKey:@"mrzText"];
-    [jsonMrz setObject:[NSNumber numberWithBool:mrzResult.isParsed] forKey:@"mrzParsed"];
-    [jsonMrz setObject:[NSNumber numberWithBool:mrzResult.isVerified] forKey:@"mrzVerified"];
-
-    return jsonMrz;
+    return @{
+        @"documentType" : [NSNumber numberWithInteger:(mrzResult.documentType + 1)],
+        @"primaryId" : mrzResult.primaryID,
+        @"secondaryId" : mrzResult.secondaryID,
+        @"issuer" : mrzResult.issuer,
+        @"dateOfBirth" : [MBSerializationUtils serializeMBDateResult:mrzResult.dateOfBirth],
+        @"documentNumber" : mrzResult.documentNumber,
+        @"nationality" : mrzResult.nationality,
+        @"gender" : mrzResult.gender,
+        @"documentCode" : mrzResult.documentCode,
+        @"dateOfExpiry" : [MBSerializationUtils serializeMBDateResult:mrzResult.dateOfExpiry],
+        @"opt1" : mrzResult.opt1,
+        @"opt2" : mrzResult.opt2,
+        @"alienNumber" : mrzResult.alienNumber,
+        @"applicationReceiptNumber" : mrzResult.applicationReceiptNumber,
+        @"immigrantCaseNumber" : mrzResult.immigrantCaseNumber,
+        @"mrzText" : mrzResult.mrzText,
+        @"mrzParsed" : [NSNumber numberWithBool:mrzResult.isParsed],
+        @"mrzVerified" : [NSNumber numberWithBool:mrzResult.isVerified]
+     };
 }
 
 @end
