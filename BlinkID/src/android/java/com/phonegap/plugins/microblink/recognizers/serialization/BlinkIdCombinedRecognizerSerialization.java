522ccb9dcec870bd6db032618d9557bfac4bc7f9
Ivan Grce <ivan.grce@microblink.com>
2019-10-17 16:42:38 +0200

    [android] updated bindings for v5.0.0

--- BlinkID/src/android/java/com/phonegap/plugins/microblink/recognizers/serialization/BlinkIdCombinedRecognizerSerialization.java
+++ BlinkID/src/android/java/com/phonegap/plugins/microblink/recognizers/serialization/BlinkIdCombinedRecognizerSerialization.java
@@ -16,7 +16,6 @@ public final class BlinkIdCombinedRecognizerSerialization implements RecognizerS
         recognizer.setFullDocumentImageExtensionFactors(BlinkIDSerializationUtils.deserializeExtensionFactors(jsonRecognizer.optJSONObject("fullDocumentImageExtensionFactors")));
         recognizer.setReturnFaceImage(jsonRecognizer.optBoolean("returnFaceImage", false));
         recognizer.setReturnFullDocumentImage(jsonRecognizer.optBoolean("returnFullDocumentImage", false));
-        recognizer.setSignResult(jsonRecognizer.optBoolean("signResult", false));
         return recognizer;
     }
 
@@ -26,20 +25,34 @@ public final class BlinkIdCombinedRecognizerSerialization implements RecognizerS
         JSONObject jsonResult = new JSONObject();
         try {
             SerializationUtils.addCommonResultData(jsonResult, result);
+            jsonResult.put("additionalAddressInformation", result.getAdditionalAddressInformation());
+            jsonResult.put("additionalNameInformation", result.getAdditionalNameInformation());
             jsonResult.put("address", result.getAddress());
+            jsonResult.put("conditions", result.getConditions());
             jsonResult.put("dateOfBirth", SerializationUtils.serializeDate(result.getDateOfBirth()));
             jsonResult.put("dateOfExpiry", SerializationUtils.serializeDate(result.getDateOfExpiry()));
             jsonResult.put("dateOfIssue", SerializationUtils.serializeDate(result.getDateOfIssue()));
-            jsonResult.put("digitalSignature", SerializationUtils.encodeByteArrayToBase64(result.getDigitalSignature()));
-            jsonResult.put("digitalSignatureVersion", (int)result.getDigitalSignatureVersion());
-            jsonResult.put("documentDataMatch", result.isDocumentDataMatch());
+            jsonResult.put("documentAdditionalNumber", result.getDocumentAdditionalNumber());
+            jsonResult.put("documentDataMatch", SerializationUtils.serializeEnum(result.getDocumentDataMatch()));
             jsonResult.put("documentNumber", result.getDocumentNumber());
             jsonResult.put("driverLicenseDetailedInfo", BlinkIDSerializationUtils.serializeDriverLicenseDetailedInfo(result.getDriverLicenseDetailedInfo()));
+            jsonResult.put("employer", result.getEmployer());
             jsonResult.put("faceImage", SerializationUtils.encodeImageBase64(result.getFaceImage()));
             jsonResult.put("firstName", result.getFirstName());
-            jsonResult.put("fullDocumentImage", SerializationUtils.encodeImageBase64(result.getFullDocumentImage()));
+            jsonResult.put("fullDocumentBackImage", SerializationUtils.encodeImageBase64(result.getFullDocumentBackImage()));
+            jsonResult.put("fullDocumentFrontImage", SerializationUtils.encodeImageBase64(result.getFullDocumentFrontImage()));
             jsonResult.put("fullName", result.getFullName());
+            jsonResult.put("issuingAuthority", result.getIssuingAuthority());
             jsonResult.put("lastName", result.getLastName());
+            jsonResult.put("maritalStatus", result.getMaritalStatus());
+            jsonResult.put("mrzResult", BlinkIDSerializationUtils.serializeMrzResult(result.getMrzResult()));
+            jsonResult.put("nationality", result.getNationality());
+            jsonResult.put("personalIdNumber", result.getPersonalIdNumber());
+            jsonResult.put("placeOfBirth", result.getPlaceOfBirth());
+            jsonResult.put("profession", result.getProfession());
+            jsonResult.put("race", result.getRace());
+            jsonResult.put("religion", result.getReligion());
+            jsonResult.put("residentialStatus", result.getResidentialStatus());
             jsonResult.put("scanningFirstSideDone", result.isScanningFirstSideDone());
             jsonResult.put("sex", result.getSex());
         } catch (JSONException e) {
--- BlinkID/src/android/java/com/phonegap/plugins/microblink/recognizers/serialization/BlinkIdRecognizerSerialization.java
+++ BlinkID/src/android/java/com/phonegap/plugins/microblink/recognizers/serialization/BlinkIdRecognizerSerialization.java
@@ -25,17 +25,32 @@ public final class BlinkIdRecognizerSerialization implements RecognizerSerializa
         JSONObject jsonResult = new JSONObject();
         try {
             SerializationUtils.addCommonResultData(jsonResult, result);
+            jsonResult.put("additionalAddressInformation", result.getAdditionalAddressInformation());
+            jsonResult.put("additionalNameInformation", result.getAdditionalNameInformation());
             jsonResult.put("address", result.getAddress());
+            jsonResult.put("conditions", result.getConditions());
             jsonResult.put("dateOfBirth", SerializationUtils.serializeDate(result.getDateOfBirth()));
             jsonResult.put("dateOfExpiry", SerializationUtils.serializeDate(result.getDateOfExpiry()));
             jsonResult.put("dateOfIssue", SerializationUtils.serializeDate(result.getDateOfIssue()));
+            jsonResult.put("documentAdditionalNumber", result.getDocumentAdditionalNumber());
             jsonResult.put("documentNumber", result.getDocumentNumber());
             jsonResult.put("driverLicenseDetailedInfo", BlinkIDSerializationUtils.serializeDriverLicenseDetailedInfo(result.getDriverLicenseDetailedInfo()));
+            jsonResult.put("employer", result.getEmployer());
             jsonResult.put("faceImage", SerializationUtils.encodeImageBase64(result.getFaceImage()));
             jsonResult.put("firstName", result.getFirstName());
             jsonResult.put("fullDocumentImage", SerializationUtils.encodeImageBase64(result.getFullDocumentImage()));
             jsonResult.put("fullName", result.getFullName());
+            jsonResult.put("issuingAuthority", result.getIssuingAuthority());
             jsonResult.put("lastName", result.getLastName());
+            jsonResult.put("maritalStatus", result.getMaritalStatus());
+            jsonResult.put("mrzResult", BlinkIDSerializationUtils.serializeMrzResult(result.getMrzResult()));
+            jsonResult.put("nationality", result.getNationality());
+            jsonResult.put("personalIdNumber", result.getPersonalIdNumber());
+            jsonResult.put("placeOfBirth", result.getPlaceOfBirth());
+            jsonResult.put("profession", result.getProfession());
+            jsonResult.put("race", result.getRace());
+            jsonResult.put("religion", result.getReligion());
+            jsonResult.put("residentialStatus", result.getResidentialStatus());
             jsonResult.put("sex", result.getSex());
         } catch (JSONException e) {
             // see https://developer.android.com/reference/org/json/JSONException
