62b779685f102e8f77450f8d90fd4779bef9e625
mparadina <milan.paradina@microblink.com>
2024-05-13 13:07:32 +0200

    Raise compileSdkVersion and targetSdkVersion in sample app

--- initCordovaSampleApp.sh
+++ initCordovaSampleApp.sh
@@ -43,8 +43,8 @@ cordova prepare
 sed -i '' 's#<platform name="android">#<platform name="android"> <edit-config file="app/src/main/AndroidManifest.xml" target="/manifest/application/activity[@android:name='\'MainActivity\'']" mode="merge"> <activity android:exported="true"/></edit-config><preference name="android-targetSdkVersion" value="33" />#g' config.xml
 sed -i '' 's#xmlns:cdv="http://cordova.apache.org/ns/1.0"#xmlns:cdv="http://cordova.apache.org/ns/1.0" xmlns:android="http://schemas.android.com/apk/res/android"#g' config.xml
 sed -i '' 's#minSdkVersion cordovaConfig.MIN_SDK_VERSION#minSdkVersion 22#g' ./platforms/android/app/build.gradle
-sed -i '' 's#compileSdkVersion cordovaConfig.SDK_VERSION#compileSdkVersion 33#g' ./platforms/android/app/build.gradle
-sed -i '' 's#targetSdkVersion cordovaConfig.SDK_VERSION#targetSdkVersion 33#g' ./platforms/android/app/build.gradle
+sed -i '' 's#compileSdkVersion cordovaConfig.SDK_VERSION#compileSdkVersion 34#g' ./platforms/android/app/build.gradle
+sed -i '' 's#targetSdkVersion cordovaConfig.SDK_VERSION#targetSdkVersion 34#g' ./platforms/android/app/build.gradle
 
 # how to run
 echo "To run iOS demo application open Xcode project $APP_NAME/platforms/ios/$APP_NAME.xcodeproj, add the NSPhotoLibraryUsageDescription key to Info.plist if the DirectAPI will be used set your development team."
